08d845147073807c08bd98f815b58c4f71be227e
DanielMa <mwldaniel_008@126.com>
2022-09-29 03:29:59 +0800

    GET/planets

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const app = require('./app')
 const http = require('http');
 
-
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app)
@@ -9,7 +8,3 @@ const server = http.createServer(app)
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 })
-
-
-
-console.log(PORT);
